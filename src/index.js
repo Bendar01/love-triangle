c3ba12135e679a31d216c4fb5cf5aa9110834438
Saha <bendar01@tut.by>
2019-02-19 20:56:10 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var counter=0;
+    for(var i=0;i < preferences.length;i++){
+
+        var one = preferences[i];
+        var two = preferences [one-1];
+        var three = preferences[two-1];
+            if(three ==i+1){
+            counter=counter+1;//counter++;
+        }
+    }
+    return Math.floor(counter/3);
+    //Метод Math.floor() возвращает наибольшее целое число, которое
+    // меньше или равно данному числу.
 };
